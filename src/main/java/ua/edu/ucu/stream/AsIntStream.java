487a8a30f31898778e1dd981a8f045c7d37ae6bc
Marko <zahart76@gmail.com>
2019-12-21 14:05:29 +0200

    Realization commit

--- src/main/java/ua/edu/ucu/stream/AsIntStream.java
+++ src/main/java/ua/edu/ucu/stream/AsIntStream.java
@@ -2,69 +2,243 @@ package ua.edu.ucu.stream;
 
 import ua.edu.ucu.function.*;
 
-public class AsIntStream implements IntStream {
+import java.util.Iterator;
 
-    private AsIntStream() {
-        // To Do
+public class AsIntStream implements IntStream {
+    protected Iterator<Integer> stream;
+
+    public AsIntStream() {
+    }
+
+    private AsIntStream(Integer[] values) {
+        this.stream = new StreamIter(values);
     }
 
     public static IntStream of(int... values) {
-        return null;
+        Integer[] ints = new Integer[values.length];
+        for (int i = 0; i < values.length; i++) {
+            ints[i] = values[i];
+        }
+        return new AsIntStream(ints);
     }
 
     @Override
     public Double average() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return (double) this.sum() / this.count();
     }
 
     @Override
     public Integer max() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (this.count() == 0) {
+            throw new IllegalArgumentException("Empty array.");
+        }
+        Iterable<Integer> iter = () -> stream;
+        Integer max = null;
+        for (Integer i : iter) {
+            if (max == null) {
+                max = i;
+            } else if (max < i) {
+                max = i;
+            }
+        }
+        return max;
     }
 
     @Override
     public Integer min() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (this.count() == 0) {
+            throw new IllegalArgumentException("Empty array.");
+        }
+        Iterable<Integer> iter = () -> stream;
+        Integer min = null;
+        for (Integer i : iter) {
+            if (min == null) {
+                min = i;
+            } else if (min > i) {
+                min = i;
+            }
+        }
+        return min;
     }
 
     @Override
     public long count() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        long counter = 0;
+        Iterable<Integer> iter = () -> stream;
+        for (Integer i : iter) {
+            counter++;
+        }
+        return counter;
     }
 
     @Override
     public Integer sum() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        if (this.count() == 0) {
+            throw new IllegalArgumentException("Empty array.");
+        }
+        Iterable<Integer> iter = () -> stream;
+        Integer sum = 0;
+        for (Integer i : iter) {
+            sum += i;
+        }
+        return sum;
     }
 
     @Override
     public IntStream filter(IntPredicate predicate) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        AsIntStream new_stream = new AsIntStream();
+        new_stream.stream = new StreamFilter(stream, predicate);
+        return new_stream;
     }
 
     @Override
     public void forEach(IntConsumer action) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Iterable<Integer> iter = () -> stream;
+        for (Integer i : iter) {
+            action.accept(i);
+        }
     }
 
     @Override
     public IntStream map(IntUnaryOperator mapper) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        AsIntStream new_stream = new AsIntStream();
+        new_stream.stream = new StreamMap(stream, mapper);
+        return new_stream;
     }
 
     @Override
     public IntStream flatMap(IntToIntStreamFunction func) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        AsIntStream new_stream = new AsIntStream();
+        new_stream.stream = new StreamFlatMap(stream, func);
+        return new_stream;
     }
 
     @Override
     public int reduce(int identity, IntBinaryOperator op) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Iterable<Integer> iter = () -> stream;
+        for (Integer i : iter) {
+            identity = op.apply(identity, i);
+        }
+        return identity;
     }
 
     @Override
     public int[] toArray() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int[] result = new int[(int) this.count()];
+        Iterable<Integer> iter = () -> stream;
+        int index = 0;
+        for (Integer i : iter) {
+            result[index] = i;
+            index++;
+        }
+        return result;
+    }
+
+    private static class StreamIter implements Iterator<Integer> {
+        private Integer[] ints;
+        private int index;
+
+        private StreamIter(Integer[] ints) {
+            this.ints = ints;
+            this.index = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (index < ints.length)
+                return true;
+            reset();
+            return false;
+        }
+
+        @Override
+        public Integer next() {
+            Integer next = ints[index];
+            index += 1;
+            return next;
+        }
+
+        public void reset() {
+            index = 0;
+        }
+    }
+
+    private static class StreamFilter implements Iterator<Integer> {
+        private Iterator<Integer> iter;
+        private IntPredicate action;
+        private Integer currentInt;
+
+        private StreamFilter(Iterator<Integer> iter, IntPredicate action) {
+            this.iter = iter;
+            this.action = action;
+        }
+
+        @Override
+        public boolean hasNext() {
+            while (iter.hasNext()) {
+                currentInt = iter.next();
+                if (action.test(currentInt)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public Integer next() {
+            return currentInt;
+        }
+    }
+
+    private static class StreamMap implements Iterator<Integer> {
+        private Iterator<Integer> iter;
+        private IntUnaryOperator operator;
+
+        private StreamMap(Iterator<Integer> iter, IntUnaryOperator operator) {
+            this.iter = iter;
+            this.operator = operator;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return iter.hasNext();
+        }
+
+        @Override
+        public Integer next() {
+            return operator.apply(iter.next());
+        }
+    }
+
+    private static class StreamFlatMap implements Iterator<Integer> {
+        private Iterator<Integer> iter;
+        private IntToIntStreamFunction operator;
+        private AsIntStream currentStream;
+
+        private StreamFlatMap(Iterator<Integer> iter, IntToIntStreamFunction operator) {
+            this.iter = iter;
+            this.operator = operator;
+        }
+
+
+        @Override
+        public boolean hasNext() {
+            if (currentStream == null || !currentStream.stream.hasNext()) {
+                currentStream = null;
+                return iter.hasNext();
+            }
+            return true;
+        }
+
+        @Override
+        public Integer next() {
+            if (currentStream == null) {
+                currentStream = (AsIntStream) operator.applyAsIntStream(iter.next());
+            }
+            return currentStream.stream.next();
+        }
+
+
     }
 
 }
